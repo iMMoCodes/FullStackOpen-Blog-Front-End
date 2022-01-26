47debeeaf2bc4e2744cc02db70b64e710ee2f12a
iMMoCodes <matti-makela@hotmail.com>
2022-01-27 01:16:41 +0200

    Tehtävä 5.14

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 test('renders title and author', () => {
@@ -16,5 +16,33 @@ test('renders title and author', () => {
   expect(component.container).toHaveTextContent('some title')
   expect(component.container).toHaveTextContent('testAuthor')
   expect(component.container).not.toHaveTextContent('testurl@test.com')
-  expect(component.container).not.toHaveValue(0)
+  expect(component.container).not.toHaveTextContent('Likes')
+})
+
+test('url and likes show after button clicked', async () => {
+  const blog = {
+    title: 'some title',
+    author: 'testAuthor',
+    url: 'testurl@test.com',
+    likes: 0,
+    user: {
+      name: 'test',
+      username: 'test',
+    },
+  }
+  const user = {
+    username: 'test',
+  }
+
+  const mockHandler = jest.fn()
+
+  const component = render(
+    <Blog blog={blog} user={user} handleClick={mockHandler} />
+  )
+
+  const button = component.getByText('view')
+  fireEvent.click(button)
+
+  expect(component.container).toHaveTextContent('testurl@test.com')
+  expect(component.container).toHaveTextContent('Likes')
 })
